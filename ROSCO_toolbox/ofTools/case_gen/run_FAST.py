85c3a2433acf2ff9918f1da0ca23e5cd29407d26
dzalkind <dzalkind@nrel.gov>
2022-08-11 13:19:35 -0600

    Final tinkering with UAE turbine

--- ROSCO_toolbox/ofTools/case_gen/run_FAST.py
+++ ROSCO_toolbox/ofTools/case_gen/run_FAST.py
@@ -274,20 +274,20 @@ if __name__ == "__main__":
 
         # RAAW FAD set up
         r.tuning_yaml   = '/Users/dzalkind/Tools/ROSCO_QED/Tune_Cases/UAE.yaml'
-        # r.wind_case_fcn = cl.user_hh
-        # r.wind_case_opts    = {
-        #     'TMax': 30.,
-        #     # 'wind_filenames': ['/Users/dzalkind/Tools/ROSCO_QED/Test_Cases/UAE_Upwind/PwrCrv.wnd']
-        #     }
-        r.wind_case_fcn = cl.simp_step
+        r.wind_case_fcn = cl.user_hh
         r.wind_case_opts    = {
-            'U_start': [16],
-            'U_end': [17],
-            # 'T_max': 120,
-            # 'T_step': 60,
-            'wind_dir': '/Users/dzalkind/Tools/ROSCO_QED/outputs/UAE_steps'
+            'TMax': 300.,
+            'wind_filenames': ['/Users/dzalkind/Tools/ROSCO_QED/Test_Cases/UAE_Upwind/test_EOG_U10.0.wnd']
             }
-        r.save_dir      = '/Users/dzalkind/Tools/ROSCO_QED/outputs/UAE_steps'
+        # r.wind_case_fcn = cl.simp_step
+        # r.wind_case_opts    = {
+        #     'U_start': [8],
+        #     'U_end': [18],
+        #     # 'T_max': 120,
+        #     # 'T_step': 60,
+        #     'wind_dir': '/Users/dzalkind/Tools/ROSCO_QED/outputs/UAE_steps'
+        #     }
+        r.save_dir      = '/Users/dzalkind/Tools/ROSCO_QED/outputs/UAE_EOG_lowTq'
         # r.control_sweep_fcn = cl.sweep_ps_percent
         r.n_cores = 1
 
--- Test_Cases/UAE_Upwind/UAE_Upwind_Rigid_WRamp_PwrCurve.fst
+++ Test_Cases/UAE_Upwind/UAE_Upwind_Rigid_WRamp_PwrCurve.fst
@@ -3,7 +3,7 @@ FAST Certification Test #10: UAE Phase VI (upwind) with no DOFs in a ramped wind
 ---------------------- SIMULATION CONTROL --------------------------------------
 True          Echo            - Echo input data to <RootName>.ech (flag)
 "FATAL"       AbortLevel      - Error level when simulation should abort (string) {"WARNING", "SEVERE", "FATAL"}
-         25   TMax            - Total run time (s)
+         300   TMax            - Total run time (s)
       0.004   DT              - Recommended module time step (s)
           2   InterpOrder     - Interpolation order for input/output time history (-) {1=linear, 2=quadratic}
           0   NumCrctn        - Number of correction iterations (-) {0=explicit calculation, i.e., no corrections}
